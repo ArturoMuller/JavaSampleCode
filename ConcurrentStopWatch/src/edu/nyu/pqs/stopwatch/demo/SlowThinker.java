a352c3681167b568fd8d578e7813e2b829f0830f
Arturo <Arturo@Arturos-MacBook-Pro.local>
2017-05-05 19:32:22 -0400

    ConcurrentStopWatch refactoring

--- ConcurrentStopWatch/src/edu/nyu/pqs/stopwatch/demo/SlowThinker.java
+++ ConcurrentStopWatch/src/edu/nyu/pqs/stopwatch/demo/SlowThinker.java
@@ -4,13 +4,13 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import edu.nyu.pqs.stopwatch.api.Stopwatch;
-import edu.nyu.pqs.stopwatch.impl.StopwatchFactory;
+import edu.nyu.pqs.stopwatch.impl.StopWatchFactory;
 
 /**
  * This is a simple program that demonstrates just some of the functionality
- * of the Stopwatch interface and StopwatchFactory class.
+ * of the StopWatch interface and StopwatchFactory class.
  * Just because this class runs successfully does not mean that the assignment is
- * complete.  It is up to you to implement the methods of Stopwatch and
+ * complete.  It is up to you to implement the methods of StopWatch and
  * StopwatchFactory.
  *
  */
@@ -28,15 +28,15 @@ public class SlowThinker {
   }
 
   /**
-   * Starts the slowthinker.
-   * It will get a stopwatch, set a number of lap times, stop the watch
+   * Starts the SlowThinker.
+   * It will get a StopWatch, set a number of lap times, stop the watch
    * and then print out all the lap times.
    *
    */
   private void go() {
     Runnable runnable = new Runnable() {
       public void run() {
-        Stopwatch stopwatch = StopwatchFactory.getStopwatch(
+        Stopwatch stopwatch = StopWatchFactory.getStopwatch(
             "ID " + Thread.currentThread().getId());
         stopwatch.start();
         for (int i = 0; i < 10; i++) {
